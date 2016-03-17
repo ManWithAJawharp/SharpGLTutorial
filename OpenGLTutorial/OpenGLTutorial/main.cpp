826eba2f10897d3aad1e550d4d7c91f2b48f0d96
Abe Vos <abe_vos@msn.com>
2016-03-17 12:52:18 +0100

    Created vertex/fragment shaders

--- OpenGLTutorial/OpenGLTutorial/main.cpp
+++ OpenGLTutorial/OpenGLTutorial/main.cpp
@@ -1,13 +1,34 @@
-#include "Dependencies\glew\glew.h"
-#include "Dependencies\freeglut\freeglut.h"
-#include <iostream>
+#include <iostream>;
+#include <stdio.h>;
+#include <stdlib.h>;
+#include <fstream>;
+#include <vector>;
+
+#include "Core\Shader_Loader.h";
+
+using namespace Core;
+
+GLuint program;
 
 void renderScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(1.0, 0.0, 0.0, 1.0);
 
+	glUseProgram(program);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
 	glutSwapBuffers();
+}
+
+void Init()
+{
+	glEnable(GL_DEPTH_TEST);
+
+	Core::Shader_Loader shaderLoader;
+	program = shaderLoader.CreateProgram("Shaders\\Vertex_Shader.glsl", "Shaders\\Fragment_Shader.glsl");
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 int main(int argc, char **argv)
@@ -19,20 +40,13 @@ int main(int argc, char **argv)
 	glutCreateWindow("Hello World");
 
 	glewInit();
-	if (glewIsSupported("GL_VERSION_4_5"))
-	{
-		std::cout << "GLEW Version is 4.5\n";
-	}
-	else
-	{
-		std::cout << "GLEW 4.5 is not supported\n";
-	}
 
-	glEnable(GL_DEPTH_TEST);
+	Init();
 
 	glutDisplayFunc(renderScene);
 
 	glutMainLoop();
+	glDeleteProgram(program);
 
 	return 0;
 }
